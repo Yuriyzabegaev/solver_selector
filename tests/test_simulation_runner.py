1330514be0d240bc1a97c1747f8bddfdbb869bb5
Yury Zabegaev <yuri.zabegaev@uib.no>
2023-09-20 11:48:03 +0000

    updated notebooks

--- tests/test_simulation_runner.py
+++ tests/test_simulation_runner.py
@@ -1,3 +1,5 @@
+from pytest import raises
+
 from tests_common import DummpyProblemContext
 
 from solver_selector.data_structures import (
@@ -12,10 +14,10 @@ from solver_selector.simulation_runner import (
 from solver_selector.solver_space import ConstantNode, KrylovSolverDecisionNode
 
 
-def generate_solver_stats(multiplier):
+def generate_solver_stats(multiplier, is_success=True):
     return NonlinearSolverStats(
-        is_converged=True,
-        is_diverged=False,
+        is_converged=is_success,
+        is_diverged=not is_success,
         nonlinear_error=[0, 0],
         iterations=[
             NonlinearIterationStats(
@@ -50,6 +52,15 @@ class DummySolver2(Solver):
         return generate_solver_stats(multiplier=10)
 
 
+class DummySolver3(Solver):
+    """This one always fails"""
+
+    def make_time_step(self, simulation: SimulationModel) -> NonlinearSolverStats:
+        assert isinstance(simulation, DummySimulation)
+        simulation.time_step_idx += 1
+        return generate_solver_stats(multiplier=100, is_success=False)
+
+
 class DummySimulation(SimulationModel):
     def __init__(self) -> None:
         self.time_step_idx = 0
@@ -63,6 +74,8 @@ class DummySimulation(SimulationModel):
             return DummySolver1()
         elif solver_name == "solver2":
             return DummySolver2()
+        elif solver_name == "solver3":
+            return DummySolver3()
         raise ValueError
 
     def is_complete(self) -> bool:
@@ -87,5 +100,18 @@ def test_simulation_runner():
     assert simulation.time_step_idx == 10
 
 
+def test_simulation_runner_bad_solver():
+    solver_space = KrylovSolverDecisionNode([ConstantNode("solver3")])
+    simulation_runner = make_simulation_runner(
+        solver_space, {"print_solver": True, "print_time": True}
+    )
+
+    simulation = DummySimulation()
+
+    with raises(ValueError):
+        simulation_runner.run_simulation(simulation)
+
+
 if __name__ == "__main__":
     test_simulation_runner()
+    test_simulation_runner_bad_solver()
